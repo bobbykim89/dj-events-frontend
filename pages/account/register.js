5441f9a8412fda946c9d5f83bb604def73a7bd2c
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-09-13 19:22:40 -0700

    added login and register page not functional yet

---
+++ pages/account/register.js
@@ -0,0 +1,81 @@
+import { FaUser } from 'react-icons/fa';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState, useEffect, useContext } from 'react';
+import Link from 'next/dist/client/link';
+import Layout from '@/components/Layout';
+import AuthContext from '@/context/AuthContext';
+import styles from '@/styles/AuthForm.module.css';
+
+const RegisterPage = () => {
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
+  const { register, error } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password !== passwordConfirm) {
+      toast.error('Passwords do not match!');
+      return;
+    }
+
+    register({ username, email, password });
+  };
+  return (
+    <Layout title='User Registration'>
+      <div className={styles.auth}>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <ToastContainer />
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='username'>Username</label>
+            <input
+              type='text'
+              value={userName}
+              id='username'
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor='email'>Email Address</label>
+            <input
+              type='email'
+              value={email}
+              id='email'
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              value={password}
+              id='password'
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor='passwordConfirm'>Confirm Password</label>
+            <input
+              type='password'
+              value={passwordConfirm}
+              id='passwordConfirm'
+              onChange={(e) => setPasswordConfirm(e.target.value)}
+            />
+          </div>
+          <input type='submit' value='Login' className='btn' />
+        </form>
+        <p>
+          Already have an account? <Link href='/account/login'>Login</Link>
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default RegisterPage;
